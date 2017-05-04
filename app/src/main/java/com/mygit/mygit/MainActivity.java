f8083336160f4a2b6b3ad31fa0e68f01345a0d85
xueqi <xueqi@guahao.com>
2017-05-04 20:13:11 +0800

    添加微信图片

--- app/src/main/java/com/mygit/mygit/MainActivity.java
+++ app/src/main/java/com/mygit/mygit/MainActivity.java
@@ -3,6 +3,7 @@ package com.mygit.mygit;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
